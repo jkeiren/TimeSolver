4a16f9ce61a08deb208f8bf47f0f84af355aca0e
Jeroen Keiren <Jeroen.Keiren@ou.nl>
2017-01-26 16:48:37 +0100

    Check the exit code of timesolver-ta.
    It turns out there are some test cases that failed, and the failure was not
    caught. This makes the failure clear.

--- TATest.py
+++ TATest.py
@@ -43,17 +43,23 @@ def runTestCase(dirName, fileName, overwrite = False):
     """Run the tool 'demo' on the testcase dirName/fileName. If overwrite is
     True, the expected output is overwritten. If overwrite is False, the output
     is compared to the file in which the expected output is stored."""
-    ret = subprocess.run([EXECUTABLE, "-d", os.path.join(dirName,fileName)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines = True)
-    resultFile = fileName + ".expectedout.gz"
+    try:
+        ret = subprocess.run([EXECUTABLE, "-d", os.path.join(dirName,fileName)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines = True, check=True)
+        resultFile = fileName + ".expectedout.gz"
 
-    if overwrite:
-        with gzip.open(os.path.join(dirName,resultFile), 'wt') as f:
-          f.write(ret.stdout)
-        result = True
-        print('[{0}] {1}/{2}'.format('\033[33mGENERATE\033[39m', dirName, resultFile))
-    else:
-        result = compare(os.path.join(dirName,resultFile), ret.stdout)
-        print('[{0}] {1}/{2}'.format('\033[32mOK\033[39m' if result else '\033[31mFAILED\033[39m', dirName, fileName))
+        if overwrite:
+            with gzip.open(os.path.join(dirName,resultFile), 'wt') as f:
+              f.write(ret.stdout)
+            result = True
+            print('[{0}] {1}/{2}'.format('\033[33mGENERATE\033[39m', dirName, resultFile))
+        else:
+            result = compare(os.path.join(dirName,resultFile), ret.stdout)
+            print('[{0}] {1}/{2}'.format('\033[32mOK\033[39m' if result else '\033[31mFAILED\033[39m', dirName, fileName))
+    except subprocess.CalledProcessError as e:
+        print('[{0}] {1}/{2}'.format('\033[31mFAILED\033[39m', dirName, fileName))
+        print('Standard error: {0}'.format(e.stderr))
+        result = False
+        
     return result
   
 def traverseTestCases(rootDir, overwrite = False):
