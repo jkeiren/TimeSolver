56ba7e290aded8cb35c83f27171a99cd7931fb0f
Jeroen Keiren <Jeroen.Keiren@ou.nl>
2017-06-07 16:32:15 +0200

    Simplify failing test case.

--- pes.timed/test/prooftest.cc
+++ pes.timed/test/prooftest.cc
@@ -11,34 +11,34 @@
 #include "parse.hh"
 #include "gtest/gtest.h"
 
-std::string FDDI4bl(
+std::string AllActBug(
     "CLOCKS: {x}\n"
     "CONTROL: {p=1}\n"
     "INITIALLY: x == 0\n"
     "PREDICATE: {X}\n"
     "START: X\n"
     "EQUATIONS: {\n"
-    "1: mu X = \\forall time\\rel[FALSE](\\AllAct(X)) && p==1\n"
+    "1: mu X = \\AllAct(X) && p==1\n"
     "}\n"
     "INVARIANT:\n"
     "	p == 1 -> x == 0\n"
     "TRANSITIONS:\n"
     "	(p==1)->(p=2);\n");
 
-TEST(ProofTest, FDDI4blTest)
+TEST(ProofTest, AllActBugTest)
 {
   pes p;
-  ASSERT_NO_THROW(parse_pes_from_string(FDDI4bl, false, p));
+  ASSERT_NO_THROW(parse_pes_from_string(AllActBug, false, p));
 
   prover_options options;
   prover pr(p, options);
   EXPECT_FALSE(pr.do_proof_init(p));
 }
 
-TEST(ProofTest, FDDI4blTestPlace)
+TEST(ProofTest, AllActBugTestPlace)
 {
   pes p;
-  ASSERT_NO_THROW(parse_pes_from_string(FDDI4bl, false, p));
+  ASSERT_NO_THROW(parse_pes_from_string(AllActBug, false, p));
 
   prover_options options;
   prover pr(p, options);
