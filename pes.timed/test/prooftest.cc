57cccaf79cb3b3789c5ab315e70e1587a0a00b30
Jeroen Keiren <Jeroen.Keiren@ou.nl>
2017-09-18 17:30:26 +0200

    Add testcases for relativized exists

--- pes.timed/test/prooftest.cc
+++ pes.timed/test/prooftest.cc
@@ -26,27 +26,6 @@ std::string AllActBug(
     "TRANSITIONS:\n"
     "	(p==1)->(p=2);\n");
 
-static
-std::string ExistsBug(
-    "#define CA 3\n"
-    "CLOCKS :{x1}\n"
-    "CONTROL :{p1(1)}\n"
-    "PREDICATE: {X}\n"
-    "START: X\n"
-    "EQUATIONS: {\n"
-    "1: nu X = (\\exists time(x1 <= 2 && \\forall time(x1 >= 4)))\n"
-    "	}\n");
-
-static
-std::string ForallRelBug(
-    "CLOCKS :{x1}\n"
-    "CONTROL :{p1}\n"
-    "PREDICATE: {X}\n"
-    "START: X\n"
-    "EQUATIONS: {\n"
-    "1: nu X = \\forall time\\rel[x1 >= 0](x1 >= 1)\n"
-    "	}\n");
-
 TEST(ProofTest, AllActBugTest)
 {
   pes p;
@@ -68,6 +47,17 @@ TEST(ProofTest, AllActBugTestPlace)
   EXPECT_FALSE(pr.do_proof_init(p, &placeholder));
 }
 
+static
+std::string ExistsBug(
+    "#define CA 3\n"
+    "CLOCKS :{x1}\n"
+    "CONTROL :{p1(1)}\n"
+    "PREDICATE: {X}\n"
+    "START: X\n"
+    "EQUATIONS: {\n"
+    "1: nu X = (\\exists time(x1 <= 2 && \\forall time(x1 >= 4)))\n"
+    "	}\n");
+
 TEST(ProofTest, ExistsBugTest)
 {
   pes p;
@@ -75,15 +65,121 @@ TEST(ProofTest, ExistsBugTest)
 
   prover_options options;
   prover pr(p, options);
+
   EXPECT_FALSE(pr.do_proof_init(p));
+
 }
 
-//TEST(ProofTest, ForallRelBugTest)
-//{
-//  pes p;
-//  ASSERT_NO_THROW(parse_pes_from_string(ForallRelBug, false, p));
+// In the following example, the property does not hold. However, the placeholder
+// that is computed for which the property does hold should be non-empty.
+static
+std::string ExistsRel(
+    "CLOCKS: {x1,x2}\n"
+    "CONTROL: {p1}\n"
+    "INITIALLY: x1 <= 2 && x2<=1\n"
+    "PREDICATE: {X}\n"
+    "START: X\n"
+    "EQUATIONS: {\n"
+    "1: mu X = \\exists time\\rel[x1 <= 3](x2==3)\n"
+    "}\n"
+    "TRANSITIONS:\n"
+    );
 
-//  prover_options options;
-//  prover pr(p, options);
-//  EXPECT_TRUE(pr.do_proof_init(p));
-//}
+TEST(ProofTest, ExistsRelTestFalse)
+{
+  pes p;
+  ASSERT_NO_THROW(parse_pes_from_string(ExistsRel, false, p));
+
+  prover_options options;
+  prover pr(p, options);
+
+  DBMList placeholder(DBM(p.initial_clock_zone()->clocks_size(), p.clocks()));
+
+  EXPECT_FALSE(pr.do_proof_init(p, &placeholder));
+  placeholder.cf();
+
+  DBM minimum_region(p.initial_clock_zone()->clocks_size(), p.clocks());
+  minimum_region.addConstraint(1,0, clock_value(2, false));
+  minimum_region.addConstraint(2,0, clock_value(1, false));
+  minimum_region.addConstraint(1,2, clock_value(0, false));
+
+  DBM maximum_region(p.initial_clock_zone()->clocks_size(), p.clocks());
+  maximum_region.addConstraint(0,1, clock_value(0, false));
+  maximum_region.addConstraint(0,2, clock_value(0, false));
+  maximum_region.addConstraint(2,0, clock_value(3, false));
+  maximum_region.addConstraint(1,2, clock_value(0, false));
+
+  DBMList minimum_placeholder(minimum_region);
+  minimum_placeholder.cf();
+  EXPECT_TRUE(minimum_placeholder <= placeholder);
+
+  DBMList maximum_placeholder(maximum_region);
+  maximum_placeholder.cf();
+  EXPECT_TRUE(placeholder <= maximum_placeholder);
+
+
+  if(!(minimum_placeholder <= placeholder) || !(placeholder <= maximum_placeholder))
+  {
+    std::cerr << "Resulting placeholder: " << placeholder << std::endl;
+    std::cerr << "Minimal placeholder: " << minimum_placeholder << std::endl;
+    std::cerr << "Maximal placeholder: " << maximum_placeholder << std::endl;
+  }
+}
+
+// In the following example, the initial region is exactly equal to the
+// part of the initial region that must be included in the placeholder.
+// Therefore, the property must hold.
+static
+std::string ExistsRelSmallRegion(
+    "CLOCKS: {x1,x2}\n"
+    "CONTROL: {p1}\n"
+    "INITIALLY: x1 <= 2 && x2<=1 && x2 - x1 >= 0\n"
+    "PREDICATE: {X}\n"
+    "START: X\n"
+    "EQUATIONS: {\n"
+    "1: mu X = \\exists time\\rel[x1 <= 3](x2==3)\n"
+    "}\n"
+    "TRANSITIONS:\n"
+    );
+
+TEST(ProofTest, ExistsRelTestTrue)
+{
+  pes p;
+  ASSERT_NO_THROW(parse_pes_from_string(ExistsRelSmallRegion, false, p));
+
+  prover_options options;
+  prover pr(p, options);
+
+  DBMList placeholder(DBM(p.initial_clock_zone()->clocks_size(), p.clocks()));
+
+  EXPECT_TRUE(pr.do_proof_init(p, &placeholder));
+  placeholder.cf();
+
+  DBM minimum_region(p.initial_clock_zone()->clocks_size(), p.clocks());
+  minimum_region.addConstraint(1,0, clock_value(2, false));
+  minimum_region.addConstraint(2,0, clock_value(1, false));
+  minimum_region.addConstraint(1,2, clock_value(0, false));
+
+  DBM maximum_region(p.initial_clock_zone()->clocks_size(), p.clocks());
+  maximum_region.addConstraint(0,1, clock_value(0, false));
+  maximum_region.addConstraint(0,2, clock_value(0, false));
+  maximum_region.addConstraint(2,0, clock_value(3, false));
+  maximum_region.addConstraint(1,2, clock_value(0, false));
+
+  DBMList minimum_placeholder(minimum_region);
+  minimum_placeholder.cf();
+  EXPECT_TRUE(minimum_placeholder <= placeholder);
+
+  DBMList maximum_placeholder(maximum_region);
+  maximum_placeholder.cf();
+  EXPECT_TRUE(placeholder <= maximum_placeholder);
+
+
+  if(!(minimum_placeholder <= placeholder) || !(placeholder <= maximum_placeholder))
+  {
+    std::cerr << "Resulting placeholder: " << placeholder << std::endl;
+    std::cerr << "Minimal placeholder: " << minimum_placeholder << std::endl;
+    std::cerr << "Maximal placeholder: " << maximum_placeholder << std::endl;
+  }
+}
+
