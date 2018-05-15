992140ee99464da4523231a24b86e3122753f4c9
Jeroen Keiren <Jeroen.Keiren@ou.nl>
2018-05-15 07:13:40 +0200

    Remove some unused methods.

--- pes.timed/Generic1DArray.hh
+++ pes.timed/Generic1DArray.hh
@@ -31,6 +31,7 @@ public:
 protected:
   /** The internal representation of the array as a sequence of bytes. */
   unsigned char *storage;
+
   /** Number of storage spaces. */
   size_type quantity;
   /** The size of each element. This is usually sizeof(short int). */
@@ -87,36 +88,6 @@ public:
     return *this;
   }
 
-  /** Adds a new element to the 1D Array at the specified index.
-   * @param element (*) The element to add to the array
-   * @param inx The index in the array to add;  0 is
-   * the first index.
-   * @return None. When completed, the array will be changed.*/
-  void add(const void *element, const size_type inx) {
-    if (inx >= quantity) {
-      std::cerr << "Generic1DArray add - index out of bounds " << std::endl;
-      exit(-1);
-    }
-    memcpy(&(storage[inx * eltSize]), element, eltSize);
-    if (!storage) {
-      std::cerr << "memcpy error - add Generic1DArray" << std::endl;
-      exit(-1);
-    }
-  }
-
-  /** Returns a reference of the element at the specified index.
-   * This function returns a reference to the original copy,
-   * not a deep copy.
-   * @param index The index of the element in the array; 0 is
-   * the first index.
-   * @return A pointer to the specified element in the array. */
-  void *fetch(const size_type index) const {
-    if (index >= quantity) {
-      return nullptr;
-    }
-    // Return address of desired element:
-    return &(storage[index * eltSize]);
-  }
 
   /** Performs a bitwise comparison of the storage of
    * both arrays.
